ab8f4fd954639031248d69bba782d5d550f2f751
gauraV <gauravsaini964@gmail.com>
2019-02-09 10:33:35 +0530

    Add auth_key model.
    Signed-off-by: gauraV <gauravsaini964@gmail.com>

--- api/models.py
+++ api/models.py
@@ -1,13 +1,100 @@
 from django.db import models
-from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
+)
 from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    """
+    Django requires that custom users define their own Manager class. By
+    inheriting from `BaseUserManager`, we get a lot of the same code used by
+    Django to create a `User`.
+
+    All we have to do is override the `create_user` function which we will use
+    to create `User` objects.
+    """
+
+    def create_user(self, username, email, password=None):
+        """Create and return a `User` with an email, username and password."""
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField(default=False)
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(unique=True, max_length=254)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
     otp = models.IntegerField(null=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    # @property
+    def __str__(self):
+        """
+        Returns a string representation of this `User`.
+
+        This string is used when a `User` is printed in the console.
+        """
+        return self.email
+
+    @property
+    def fullname(self):
+        fullname = self.first_name + ' ' + self.last_name
+        return fullname
+
     class Meta:
         db_table = 'user'
+
+
+class AuthKey(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    key = models.CharField(max_length=50, null=False, blank=False)
+    desc = models.CharField(null=True, max_length=100)
+    starts_at = models.DateTimeField(null=False, blank=False)
+    ends_at = models.DateTimeField(null=False, blank=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'auth_key'
+
+    def __str__(self):
+        return self.name
