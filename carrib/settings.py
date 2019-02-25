ff172cc40f4f159c448a343539328208a2968b07
gauraV <gauravsaini964@gmail.com>
2019-02-25 10:20:06 +0530

    Finalize heroku changes.
    Signed-off-by: gauraV <gauravsaini964@gmail.com>

--- carrib/settings.py
+++ carrib/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# enable heroku
+django_heroku.settings(locals())
 
 # Application definition
 
