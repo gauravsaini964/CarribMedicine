c3d10b0ec95a9d8a1bd673fedbd05baf66a564e0
gauraV <gauravsaini964@gmail.com>
2019-02-26 11:46:14 +0530

    Addons in quiz API for practice paper.
    Signed-off-by: gauraV <gauravsaini964@gmail.com>

--- api/views/Practice.py
+++ api/views/Practice.py
@@ -10,7 +10,8 @@ import datetime
 
 # Models Imports.
 from api.models import (
-    User, Subjects, Questions, Choices, QuestionsSubject, QuestionsChoices, Quiz, QuizQuestions)
+    User, Subjects, Questions, Choices, QuestionsSubject, QuestionsChoices, Quiz, QuizQuestions, PracticePapers,
+    QuestionsQuestionBank, UserSubjectScore, UserPracticePaperScore)
 
 # Serializer Imports.
 
@@ -20,19 +21,34 @@ class QuizInitializeView(APIView):
     def post(request):
         user = 4
         subject_id = request.POST.get('subject_id', None)
-        if not subject_id:
-            res = {'message': 'Subject ID not found'}
+        practice_paper_id = request.POST.get('practice_paper_id', None)
+        if not subject_id or practice_paper_id:
+            res = {'message': 'Subject ID/ Paper ID not found'}
             return Response(res, status.HTTP_200_OK)
-        subject = Subjects.objects.filter(id=subject_id).values().first()
-        questions_list = QuestionsSubject.objects.filter(subject_id=subject_id, question__flag=1).values_list(
-            'question_id', flat=True)
-        questions = Questions.objects.filter(
-            id__in=questions_list, flag=True, question_type=1, text__isnull=False).order_by('?')[:10]
-        final_question_list = questions.values_list('id', flat=True)
-        
-        quiz_obj = Quiz.objects.create(
-            user_id=user, quiz_type='practice', result_set={}, entity_id=subject['id'], entity_type='subject',
-            total_questions=len(final_question_list))
+
+        if subject_id:
+            subject = Subjects.objects.filter(id=subject_id).values().first()
+            questions_list = QuestionsSubject.objects.filter(subject_id=subject_id, question__flag=1).values_list(
+                'question_id', flat=True)
+            questions = Questions.objects.filter(
+                id__in=questions_list, flag=True, question_type=1, text__isnull=False).order_by('?')[:10]
+            final_question_list = questions.values_list('id', flat=True)
+            
+            quiz_obj = Quiz.objects.create(
+                user_id=user, quiz_type='learn', result_set={}, entity_id=subject['id'], entity_type='subject',
+                total_questions=len(final_question_list))
+
+        if practice_paper_id:
+            paper_obj = PracticePapers.objects.filter(id=practice_paper_id).first()
+            questions_list = QuestionsQuestionBank.objects.filter(
+                question_bank_id=paper_obj.question_bank_id).values_list('question_id', flat=True)
+            questions = Questions.objects.filter(
+                id__in=questions_list, flag=True, question_type=1, text__isnull=False).order_by('?')[:10]
+            final_question_list = questions.values_list('id', flat=True)
+            quiz_obj = Quiz.objects.create(
+                user_id=user, quiz_type='practice', result_set={}, entity_id=paper_obj.id, entity_type='practice_paper',
+                total_questions=len(final_question_list))
+
         final_questions = []
         for question_id in final_question_list:
             quiz_questions = QuizQuestions.objects.create(quiz_id=quiz_obj.id, question_id=question_id)
@@ -43,6 +59,7 @@ class QuizInitializeView(APIView):
                     'choice_id', 'is_correct')
             temp_dict = {'question': question_obj, 'choices': question_choices, 'quiz_question_id': quiz_questions.id}
             final_questions.append(temp_dict)
+
         final_quiz_data = {'quiz_id': quiz_obj.id, 'questions': final_questions}
         res = {
             'message': 'Quiz generated successfully',
@@ -97,9 +114,10 @@ class QuizAnswersView(APIView):
         quiz_quest_obj.response_time = response_time
         quiz_quest_obj.save()
         quiz_obj.save()
+        quiz_quest_status = QuizQuestions.objects.filter(id=quiz_question_id).values().order_by('id')
         res = {
             'message': 'Answer Posted Successfully',
-            'result': {'answered_correctly': answered_correctly}}
+            'result': {'answered_correctly': answered_correctly, 'question_status': quiz_quest_status}}
         return Response(res, status.HTTP_200_OK)
 
 
@@ -108,12 +126,13 @@ class QuizCompletionView(APIView):
     @staticmethod
     def post(request):
 
+        user = request.requested_by
         quiz_id = request.POST.get('quiz_id', None)
         quiz_obj = Quiz.objects.filter(id=quiz_id).first()
-
+        
         if quiz_obj.is_complete is False:
             
-            percentage = (float(quiz_obj.marks)/ float(quiz_obj.total_questions)) * 100.00
+            percentage = (float(quiz_obj.marks) / float(quiz_obj.total_questions)) * 100.00
 
             quiz_obj.is_complete = True
             quiz_obj.percentage = percentage
@@ -125,6 +144,28 @@ class QuizCompletionView(APIView):
             quiz_result_set_modify['correct_questions'] = quiz_obj.correct_questions
             quiz_result_set_modify['incorrect_questions'] = quiz_obj.incorrect_questions
             quiz_result_set_modify['total_attempted'] = quiz_obj.questions_attempted
+
+            if quiz_obj.entity_type == 'subject':
+                obj, created = UserSubjectScore.objects.update_or_create(user_id=user, subject_id=quiz_obj.entity_id)
+                if created:
+                    created.score = quiz_obj.marks
+                    created.total_score = quiz_obj.total_questions
+                    created.save()
+                else:
+                    obj.score += quiz_obj.marks
+                    obj.total_score += quiz_obj.total_questions
+                    obj.save()
+            elif quiz_obj.entity_type == 'practice_paper':
+                obj, created = UserPracticePaperScore.objects.update_or_create(
+                    user_id=user, practice_paper_id=quiz_obj.entity_id)
+                if created:
+                    created.score = quiz_obj.marks
+                    created.total_score = quiz_obj.total_questions
+                    created.save()
+                else:
+                    obj.score = quiz_obj.marks
+                    obj.total_score = quiz_obj.total_questions
+                    obj.save()
 
         res = {'message': 'Result compiled successfully', 'result': quiz_obj.result_set}
         quiz_obj.save()
